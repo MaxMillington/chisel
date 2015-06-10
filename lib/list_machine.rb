187542d580b87a68e918178b4008fab4cfa421a0
MaxMillington <maxmillington89@gmail.com>
2015-06-10 17:08:09 -0600

    Add the ability to render ordered list
    We refactored our ordered list handling, as it was handling a lot of state in
    order to determine when a list ends, and the enumerable module can handle that
    for us.
    
    We're doing a lot of leg work to determine if there is a numeric prefix,
    because the project spec says we aren't allowed to use regular expressions.

--- lib/list_machine.rb
+++ lib/list_machine.rb
@@ -2,25 +2,67 @@ require 'pry'
 
 class ListMachine
   def self.convert(input)
-    new_input = input.split("/n")
-    flag = 0
-    converted_inputs = []
-    new_input.each.with_index do |element,index|
-      if element.start_with?("* ") && flag == 0
-        flag += 1
-        formatted_element = "<ul><li>#{element.slice(2..-1)}</li>"
-        converted_inputs << formatted_element
-      elsif element.start_with?("* ") && flag > 0
-        converted_inputs << "<li>#{element.slice(2..-1)}</li>"
+    new.convert(input)
+  end
+
+  def convert(input)
+    lines = input.split("\n")
+    groups_of_lists = lines.slice_when do |line, next_line|
+      is_a_list_item?(line) != is_a_list_item?(next_line)
+    end
+    render_lists(groups_of_lists).join("\n")
+  end
+
+  private
+
+  def render_lists(groups_of_lists)
+    groups_of_lists.flat_map do |group|
+      if is_a_list_item?(group.first)
+        render_list(group)
       else
-        converted_inputs << element
-      end
-      if flag > 0 && (index+1 == new_input.length)
-      formatted_element = "</ul>"
-      converted_inputs << formatted_element
+        group
       end
     end
-    converted_inputs.join("")
+  end
+
+  def wrap_in_li_tags(group)
+    group.map do |line|
+      line_without_prefix = line.split(" ").drop(1).join(" ")
+      "<li>#{line_without_prefix}</li>"
+    end
+  end
+
+  def is_a_list_item?(line)
+    ordered_list_item?(line) || unordered_list_item?(line)
+  end
+
+  def render_list(group)
+    if ordered_list_item?(group.first)
+      tag = "ol"
+    else
+      tag = "ul"
+    end
+    ["<#{tag}>"] + wrap_in_li_tags(group) + ["</#{tag}>"]
+  end
+
+  def prefix_for(line)
+    line.split(" ").first
+  end
+
+  def ordered_list_item?(line)
+    prefix = prefix_for(line)
+    prefix_except_trailing_dot = prefix[0..-2]
+    is_numeric?(prefix_except_trailing_dot) && prefix.end_with?(".")
+  end
+
+  def unordered_list_item?(line)
+    prefix_for(line) == "*"
+  end
+
+  def is_numeric?(string)
+    string.chars.all? do |char|
+      ("0".."9").include?(char)
+    end
   end
 end
 
--- test/list_machine_test.rb
+++ test/list_machine_test.rb
@@ -5,15 +5,15 @@ require_relative '../lib/list_machine'
 class ListMachineTest < Minitest::Test
 
   def test_it_can_order_a_one_item_list
-    input = "Meats: /n* bresaola"
+    input = "Meats: \n* bresaola"
     output = ListMachine.convert(input)
-    assert_equal "Meats: <ul><li>bresaola</li></ul>", output
+    assert_equal "Meats: \n<ul>\n<li>bresaola</li>\n</ul>", output
   end
 
   def test_it_recognizes_single_star_as_unordered_list
-    input = "My special meats: /n* bresaola/n* prosciutto/n* finocchiona"
+    input = "My special meats: \n* bresaola\n* prosciutto\n* finocchiona"
     output = ListMachine.convert(input)
-    assert_equal "My special meats: <ul><li>bresaola</li><li>prosciutto</li><li>finocchiona</li></ul>", output
+    assert_equal "My special meats: \n<ul>\n<li>bresaola</li>\n<li>prosciutto</li>\n<li>finocchiona</li>\n</ul>", output
   end
 
   def test_it_will_not_screw_up_my_emphasizer
@@ -29,9 +29,33 @@ class ListMachineTest < Minitest::Test
   end
 
   def test_it_can_handle_emphasis_and_embolden_in_a_list
-    input = "My special meats: /n* *bresaola*/n* **prosciutto**/n* finocchiona"
+    input = "My special meats: \n* *bresaola*\n* **prosciutto**\n* finocchiona"
     output = ListMachine.convert(input)
-    assert_equal "My special meats: <ul><li>*bresaola*</li><li>**prosciutto**</li><li>finocchiona</li></ul>", output
+    assert_equal "My special meats: \n<ul>\n<li>*bresaola*</li>\n<li>**prosciutto**</li>\n<li>finocchiona</li>\n</ul>", output
+  end
+
+  def test_it_can_handle_an_ordered_list
+    input = "I like: \n1. cows"
+    output = ListMachine.convert(input)
+    assert_equal "I like: \n<ol>\n<li>cows</li>\n</ol>", output
+  end
+
+  def test_it_can_handle_a_longer_ordered_list
+    input = "I like: \n1. cows\n2. pigs"
+    output = ListMachine.convert(input)
+    assert_equal "I like: \n<ol>\n<li>cows</li>\n<li>pigs</li>\n</ol>", output
+  end
+
+  def test_it_ignores_fancy_8th_grade_essay_form_prefixes
+    input = "I like: \n1a. stuff"
+    output = ListMachine.convert(input)
+    assert_equal "I like: \n1a. stuff", output
+  end
+
+  def test_it_can_handle_numbers_greater_than_9
+    input = "I like: \n11. other things"
+    output = ListMachine.convert(input)
+    assert_equal "I like: \n<ol>\n<li>other things</li>\n</ol>", output
   end
 
 end
