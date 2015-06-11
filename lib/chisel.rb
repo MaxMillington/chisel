2f00ae1a6f24fdce1fa7d5bf8df1a522a51050ca
MaxMillington <maxmillington89@gmail.com>
2015-06-11 10:19:24 -0600

    things are on track. Started a link machine. Haven't figure out how to call everything from the command line yet

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,10 +1,10 @@
 #read input from input.md, apply the renderer.rb file to it, and send it to output_file.html.
 
-require 'renderer'
-require 'chunk_maker'
-require 'header_machine'
-require 'list_machine'
-require 'paragraph_machine'
+require_relative 'renderer'
+require_relative 'chunk_maker'
+require_relative 'header_machine'
+require_relative 'list_machine'
+require_relative 'paragraph_machine'
 
 class Chisel
   def initialize(input)
@@ -19,13 +19,33 @@ class Chisel
     @renderer.render
   end
 end
+#
+# if __FILE__ == $0
+#   $LOAD_PATH.unshift(File.expand_path("..", __FILE__))
+#
+#   input = File.read(ARGV[0])
+#   output = File.open(ARGV[1], "w")
+#
+#   chisel = Chisel.new(input)
+#   output.write(chisel.result)
+# end
 
-if __FILE__ == $0
-  $LOAD_PATH.unshift(File.expand_path("..", __FILE__))
 
-  input = File.read(ARGV[0])
-  output = File.open(ARGV[1], "w")
 
-  chisel = Chisel.new(input)
-  output.write(chisel.result)
-end
+chisler = Chisel.new('# My Life in Desserts
+
+## Chapter 1: The Beginning
+
+    "You just *have* to try the cheesecake," he said. "Ever since it appeared in
+**Food & Wine** this place has been packed every night."
+
+* Things are so great
+* The tests are passing
+
+These are my favorite movies:
+
+1. Vertigo
+2. 2001
+3. The Third Man')
+
+chisler
---
+++ lib/link_machine.rb
@@ -0,0 +1,18 @@
+class LinkMachine
+  def self.convert(input)
+    input.split("](")
+  end
+
+
+end
+
+
+
+
+
+
+
+'[Apocalypse Now](http://www.imdb.com/title/tt0078788/)'
+
+
+'<a href="http://www.imdb.com/title/tt0078788/">Apocalypse Now</a>'
